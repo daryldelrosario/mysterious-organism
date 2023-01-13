d6b582ddc19dd5173055a5db4c05df051914a35f
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-01-12 19:55:37 -0500

    Work on compareDNA function

--- main.js
+++ main.js
@@ -17,12 +17,34 @@ const mockUpStrand = () => {
 const pAequorFactory = (specimenNum, dna) => {
   return {
     specimenNum,
-    dna
+    dna,
+    mutate() {
+      const randomBasePosition = Math.floor(Math.random() * 15);
+      const selectedBase = dna[randomBasePosition];
+      let changeBase = selectedBase;
+      while(changeBase === selectedBase) {
+        changeBase = returnRandBase();
+      }
+      console.log("Base Position at: " + randomBasePosition); // Debug
+      console.log("We are changing: " + selectedBase); // Debug
+      console.log("With " + changeBase); // Debug
+      dna[randomBasePosition] = changeBase;
+
+      return dna;
+    }
   }
 }
 
-console.log(pAequorFactory(1, mockUpStrand()));
 
+// Test Functions
+const strand1 = pAequorFactory(1, mockUpStrand());
+console.log("This is strand1: " + strand1.dna.join(" "));
+
+const strand2 = pAequorFactory(2, mockUpStrand());
+console.log("This is strand2: " + strand2.dna.join(" "));
+
+const strand1m = strand1.mutate();
+console.log("This is strand1m: " + strand1m.join(" "));
 
 
 
